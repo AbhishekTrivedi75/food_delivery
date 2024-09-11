402de875694394ebb453a6bf565e63fb226524fb
Abhishek Trivedi <137615533+AbhishekTrivedi75@users.noreply.github.com>
2024-09-11 16:36:39 +0530

    Update StoreContext.jsx

--- admin/src/context/StoreContext.jsx
+++ admin/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) =>{
-    const url = 'http://localhost:4000'; 
+    const url = 'https://food-delivery-backend-3a5g.onrender.com'; 
     const [token,setToken]=useState("")
     const [showLogin,setShowLogin]=useState(true);
     useEffect(()=>{
